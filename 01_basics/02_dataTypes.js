58f92041c5758eac0fbbd83ad26bba86fc2047a7
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-02-24 05:43:06 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,47 @@
+"use strict"; //treats all JS code as newer version of JavaScript
+
+// alert("hello")
+// we are using node JS not browser
+
+
+//Exmaple of bad code readability
+/*
+console.log(3 + 3); console.log("Sameer")
+
+console.log(3
+    + 
+    3)
+*/
+
+// High code readability should be the priority
+// example :-
+
+console.log(33 + 33)
+console.log("Sameer")
+
+
+// Important documentations of JS
+// 1 => MDN documentation for JS => for good readability
+// 2 => https://tc39.es/ecma262/     => JS standard docs
+
+
+// Primitive data types :-
+
+// number => 2^23
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (representation of emptiness)
+// undefined => (value not assigned)
+// symbol => helps to find uniqueness
+
+// object
+
+let name = "Sameer"
+let age = 18
+let loggedIn = true
+
+console.log(typeof "Sameer")
+console.log(typeof null)
+console.log(typeof undefined)
+
