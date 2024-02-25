1c178ad0b81f5bc706ffbb2a7ef1015e6db59e4d
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-02-25 17:09:22 +0000

    Math and Numbers

--- 01_basics/06_numbers_and_math.js
+++ 01_basics/06_numbers_and_math.js
@@ -1,32 +1,32 @@
-// const score = 400
-// console.log(score);
+const score = 400
+console.log(score);
 
-// const balance = new Number(100)
-// console.log(balance);
+const balance = new Number(100)
+console.log(balance);
 
-// console.log(balance.toString());
+console.log(balance.toString());
 
-// console.log(balance.toFixed(2));
-
-
-// const otherNumber = 23.8966
-
-// console.log(otherNumber.toPrecision(3));       // 23.9
+console.log(balance.toFixed(2));
 
 
-// const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));  // 10,00,000
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3));       // 23.9
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));  // 10,00,000
 
 
 // ++++++++++++++++++ Maths +++++++++++++++++
 
-// console.log(Math)
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));        // 5
-// console.log(Math.floor(4.9));       // 4
-// console.log(Math.min(2, 3, 4, 5));
-// console.log(Math.max(2, 3, 4, 5));
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));        // 5
+console.log(Math.floor(4.9));       // 4
+console.log(Math.min(2, 3, 4, 5));
+console.log(Math.max(2, 3, 4, 5));
 
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
