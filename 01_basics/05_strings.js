8bcbbdbcc173752dfa08dd3ec50525feb3af0b7e
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-02-25 16:05:57 +0000

    String in JS

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Sameer"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('SameerKr')
