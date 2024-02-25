ff83262881e1de7a9b0311929e217ca6d2379859
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-02-25 18:25:05 +0000

    Dates in JS

--- 01_basics/07_dates_in_js.js
+++ 01_basics/07_dates_in_js.js
@@ -1,14 +1,14 @@
 // Dates
 
 let myDate = new Date()
-// console.log(typeof myDate);
+console.log(typeof myDate);
 
-// console.log(myDate.toString());     // Sun Feb 25 2024 18:04:46 GMT+0000 (Coordinated Universal Time)
-// console.log(myDate.toDateString()); // Sun Feb 25 2024
-// console.log(myDate.toISOString());  // 2024-02-25T18:05:27.773Z
-// console.log(myDate.toJSON());       // 2024-02-25T18:05:56.512Z
-// console.log(myDate.toLocaleDateString());  // 2/25/2024
-// console.log(myDate.toLocaleString());   // 2/25/2024, 6:07:38 PM
+console.log(myDate.toString());     // Sun Feb 25 2024 18:04:46 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()); // Sun Feb 25 2024
+console.log(myDate.toISOString());  // 2024-02-25T18:05:27.773Z
+console.log(myDate.toJSON());       // 2024-02-25T18:05:56.512Z
+console.log(myDate.toLocaleDateString());  // 2/25/2024
+console.log(myDate.toLocaleString());   // 2/25/2024, 6:07:38 PM
 
 let myCreatedDate = new Date(2023, 0, 23)
 console.log(myCreatedDate.toDateString());  // Mon Jan 23 2023
