b9bac16cbf22d36118c16f6beb5ae128da5826fa
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-03-05 11:40:23 +0000

    For loops

--- 05_iteration/01_for_loop.js
+++ 05_iteration/01_for_loop.js
@@ -5,3 +5,45 @@ for (let i = 0; i <= 10 ; i++) {
     console.log(element);
     
 }
+
+/*
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value : ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        console.log(i + '*' + j + ' = ' + i*j);
+    }
+    
+}
+
+*/
+
+let myArray = ['Flash', "Batman", "Superman"];
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5) {
+        console.log("Detected 5")
+        break
+    }
+    console.log(`Value of i is : ${index}`)
+    
+}
+
+
+// continue (skips the remaing code for one time after execution)
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5) {
+        console.log("Detected 5")
+        continue
+    }
+    console.log(`Value of i is : ${index}`)
+    
+}
