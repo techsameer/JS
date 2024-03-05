3de94654b7b81bc525fa591101d30da830d6e5e5
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-03-05 13:31:40 +0000

    For in loop

---
+++ 05_iteration/04_for_in.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js : "JavaScript",
+    cpp : "C++",
+    rb : "Ruby",
+    swift : "Swift by apple"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+const programming = ["js", "ruby", "python", "java", "cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// Maps are non iterable
