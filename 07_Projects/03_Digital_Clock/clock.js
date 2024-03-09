7c67d08ade114ccfe5527a8d669c02c5a0f8d4e0
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-03-09 16:42:36 +0000

    Digital Clock Project

---
+++ 07_Projects/03_Digital_Clock/clock.js
@@ -0,0 +1,7 @@
+const clock = document.getElementById('clock')
+
+setInterval(function(){
+   // console.log(date.toLocaleTimeString())
+   let date = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000)
