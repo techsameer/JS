1ac8ef214c6be76ab0f191529a0e55c8c8164d94
Sameer Kumar <59678074+techsameer@users.noreply.github.com>
2024-03-13 13:36:31 +0000

    OOP in JS

--- 12_classes_and_oop/01_oop.js
+++ 12_classes_and_oop/01_oop.js
@@ -13,3 +13,22 @@ const user = {
 console.log(user.username);
 // console.log(user.getUserDetails());
 console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    this.greetings = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+    return this
+}
+
+const userOne = new User("sameer", 12, true)
+const userTwo = new User("test", 11, false)
+
+
+console.log(userOne.constructor);
+// console.log(userTwo);
